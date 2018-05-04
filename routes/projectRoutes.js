ffd65cc1d5fb8917f653ed8a8082cc402d1dd588
Mark Wong <fmarkwong@gmail.com>
2018-05-04 10:27:23 -0700

    project routes

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -5,9 +5,42 @@ const projectsDb = require('../data/helpers/projectModel');
 
 const asyncHandler = fn => (req, res, next) => fn(req, res, next).catch(next);
 
-router.get('/', asyncHandler(async (req, res) => {
+// Get all
+router.get('/', asyncHandler(async (req, res, next) => {
   const projects = await projectsDb.get();
   res.status(200).json(projects);
+}));
+
+// Create
+router.post('/', asyncHandler(async (req, res, next) => {
+  const newProjectData = req.body;
+  const newProject = await projectsDb.insert(newProjectData);
+  res.status(200).json(newProject);
+}));
+
+// Read 
+router.get('/:id', asyncHandler(async (req, res, next) => {
+  const project = await projectsDb.get(req.params.id);
+  if (!project) throw `Project id ${req.params.id} not found`;
+  res.status(200).json(project);
+
+}));
+
+// Update
+router.put('/:id', asyncHandler(async (req, res, next) => {
+  const updatedProjectData = req.body;
+  const updatedProject = await projectsDb.update(req.params.id, updatedProjectData);
+  if (!updatedProjectData) throw `Project id ${req.params.id} not found`;
+  res.status(200).json(updatedProject);
+
+}));
+
+// Delete 
+router.delete('/:id', asyncHandler(async (req, res, next) => {
+  const numRowsAffected = await projectsDb.remove(req.params.id);
+  if (!numRowsAffected) throw `Project id ${req.params.id} not found`;
+  res.status(200).json(numRowsAffected);
+
 }));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json());
+
 const projectRoutes = require('./routes/projectRoutes');
 
 server.listen(5000, () => console.log("\n server listening on port 5000"));
 
 server.use('/api/projects', projectRoutes);
+
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).json(err);
+}
